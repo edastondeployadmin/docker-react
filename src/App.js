9c7dcaa601cbe5c16cde838981a22131aaef052b
Edward Ad <edwardad@MacBook-Pro.local>
2020-09-18 09:17:16 -0400

    I was changed on feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye There </p>
+        <p>Bye There I was changed on featuere</p>
         <a
           className="App-link"
           href="https://reactjs.org"
